80a5d2f8d5f0f676d01ddb1ef262b20270fd859f
John Oseni <johnoseni26@gmail.com>
2022-05-23 20:41:05 +0100

    fixed it all up

--- pages/api/middleware/middleware.js
+++ pages/api/middleware/middleware.js
@@ -3,3 +3,17 @@ import multiparty from 'multiparty'
 
 const middleware = nextConnect()
 
+middleware.use((req, res, next) => {
+  const form = new multiparty.Form()
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      console.log(err)
+      next()
+    }
+    req.body = fields
+    req.files = files
+    next()
+  })
+})
+
+export default middleware
