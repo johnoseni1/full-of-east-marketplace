66bfc67a0c10855d320849938fab3584f4b7fbd1
John Oseni <johnoseni26@gmail.com>
2022-05-23 21:55:17 +0100

    fixed it all up

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import '../styles/globals.css'
 import '../styles/css/animate.min.css'
 import '../styles/css/preloader.css'
-import '../styles/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/css/meanmenu.css'
-// import '../styles/css/owl.carousel.min.css'
+import '../styles/css/owl.carousel.min.css'
 import '../styles/css/swiper-bundle.css'
 import '../styles/css/backToTop.css'
 import '../styles/css/magnific-popup.css'
@@ -11,6 +12,8 @@ import '../styles/css/nice-select.css'
 import '../styles/css/flaticon.css'
 import '../styles/css/default.css'
 import '../styles/css/style.css'
+import 'owl.carousel'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
